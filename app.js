35c1a6e14cf14ba0c2f9509e9312cec5db671a0b
Harshitn24 <125255820+Harshitn24@users.noreply.github.com>
2025-04-23 11:50:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
     {
-        origin: ["https://civil-rosalynd-harshitnavadiya-d23fda25.koyeb.app"],
+        origin: ["https://postpal-one.vercel.app"],
         methods: ["POST","GET"],
         credentials:true
     }
